075ed17f84268e2079e1db5f9be159e7e560722d
unknown <sarita-coder>
2016-05-19 17:31:11 -0500

    agregando

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(function(){
 	$("#reloj").on(hora());
+	//$("#moden").on(pais());
 });
 
 function hora(){
@@ -16,14 +17,60 @@ function hora(){
 	$("#reloj").html(h+":"+m+":"+s);
 	$("#fecha").html(nomDia[day]+", "+tiempo.getDate()+" de "+meses[mes]);
 }
+//$(".moden").change(pais);
+var ArrayPais= [{
+				nombre:"Chicago",
+				horario: "0",
+				},{
+				nombre:"Sao Paulo",
+				horario: "+2",
+				},{
+				nombre:"Santiago",
+				horario: "+2",
+				},{
+				nombre:"Mexico D.F",
+				horario: "-1",
+				},{
+				nombre:"Caracas",
+				horario: "+1",
+				},{
+				nombre:"Brasilia",
+				horario: "+2",
+				},{
+				nombre:"Quito",
+				horario: "0",
+				},{
+				nombre:"Guayaquil",
+				horario: "0",
+				},{
+				nombre:"Santa Marta",
+				horario: "1",
+				},{
+				nombre:"Bogota",
+				horario: "1",
+				}];
 
-function pais(){
-	
+function horaPais(){
+	var Huso=$(".label-modal").val();
+
 
 }
+// function pais (){
+// 	if($(this).prop("checked")==true){
+// 		$(".moden")
+// 		$(".paises").append("<p id='p"+num+"'>hola</p>");
 
-
-
+// 	} else {
+// 		$("#p"+num).remove();
+// 	}
+// }
+	// <p class='p"+num+"'>
+	// $('#nun'+num)
+$(document).ready(function(){
+	$(".moden").click(function(){
+		alert($(this).attr("data"));
+	});
+});
 
 
 
